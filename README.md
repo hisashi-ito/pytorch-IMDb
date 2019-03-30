55e7eda007f1df08a16a3c80cbfde5a0c224e7aa
Hisashi Itoh <it.hisas@gmail.com>
2019-03-30 14:15:56 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pytorch-IMDb
 IMDb classifier using pytorch
+
+* Large Movie Review Dataset  
+http://ai.stanford.edu/~amaas/data/sentiment/
---
+++ tokenizer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#【tokenizer】
+#
+# 概要: NLTK(https://www.nltk.org/) を利用したtokenizer を実装します
+#       NLTKの parser を利用時は事前に以下のコマンドを実行しておく
+#
+#       $ python3
+#       >>> import nltk
+#       >>> nltk.download('punkt')
+#
+# 更新履歴:
+#          2019.03.30 新規作成
+#
+import nltk
+
+class Tokenizer(object):
+    def __init__(self):
+        pass
+    
+    def tokenize(self, text):
+        try:
+            tokens = nltk.word_tokenize(text)
+        except:
+            raise Exception("NLTK 形態素解析で失敗しました: {0}".format(text))
+        forms = []
+        for token in tokens:
+            if len(token) == 0:
+                continue
+            forms.append(token)
+        return forms
+
+    
+if __name__ == '__main__':
+     text = "I saw 'Liz and the blue bird' at Shinjuku Piccadilly."
+     m = Tokenizer()
+     ret = m.tokenize(text)
+     print(ret)
+     
